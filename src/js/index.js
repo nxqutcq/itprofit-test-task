88e9ba10def3370cf41228bc5539ba8820e7e743
vlad <vnelipovich63@gmail.com>
2025-01-25 14:47:05 +0300

    refactor: index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,51 +5,61 @@ import { initializeModal } from './modal'
 import '../scss/styles.scss'
 import '../scss/_form.scss'
 
+const API_URL = 'http://localhost:5000/submit'
+const TOAST_MESSAGES = {
+  FORM_SUCCESS: 'Форма отправлена',
+  FORM_ERROR: 'Форма содержит ошибки. Проверьте введенные данные.',
+  SERVER_ERROR: 'Ошибка отправки данных на сервер: ',
+  VALIDATION_ERROR: 'Ошибка валидации формы.',
+}
+
 initializeModal()
 
 const form = document.querySelector('.contactForm')
-
 const validateForm = initializeValidation(form)
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault()
 
-  const isValid = validateForm()
-
-  if (isValid) {
-    const formData = new FormData(form)
-
-    try {
-      const response = await submitForm(
-        'http://localhost:5000/submit',
-        formData
-      )
-
-      if (response.status === 'success') {
-        form.reset()
-        showToast('Форма отправлена')
-      } else if (response.status === 'error') {
-        handleValidationErrors(response.fields)
-        showToast('Ошибка валидации формы.', 'error')
-      }
-    } catch (error) {
-      showToast('Ошибка отправки данных на сервер: ' + error.message, 'error')
-    }
+  if (validateForm()) {
+    await handleFormSubmit(form)
   } else {
-    showToast('Форма содержит ошибки. Проверьте введенные данные.', 'error')
+    showToast(TOAST_MESSAGES.FORM_ERROR, 'error')
   }
 })
+
+async function handleFormSubmit(form) {
+  const formData = new FormData(form)
+
+  try {
+    const response = await submitForm(API_URL, formData)
+
+    if (response.status === 'success') {
+      form.reset()
+      showToast(TOAST_MESSAGES.FORM_SUCCESS)
+    } else if (response.status === 'error') {
+      handleValidationErrors(response.fields)
+      showToast(TOAST_MESSAGES.VALIDATION_ERROR, 'error')
+    }
+  } catch (error) {
+    showToast(TOAST_MESSAGES.SERVER_ERROR + error.message, 'error')
+  }
+}
 
 function handleValidationErrors(fields) {
   for (const field in fields) {
     const input = document.getElementById(field)
     if (input) {
-      const error = input.nextElementSibling
-      if (error && error.classList.contains('form__error')) {
-        error.textContent = fields[field]
-        error.style.display = 'block'
-      }
-      input.classList.add('input--error')
+      showValidationError(input, fields[field])
     }
   }
+}
+
+function showValidationError(input, message) {
+  const error = input.nextElementSibling
+  if (error && error.classList.contains('form__error')) {
+    error.textContent = message
+    error.style.display = 'block'
+  }
+  input.classList.add('input--error')
 }
